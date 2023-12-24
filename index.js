87551733333683aebc57845562687f664c240a48
AbhishekPSingh07 <79076050+AbhishekPSingh07@users.noreply.github.com>
2023-12-24 21:20:44 +0530

    Changes

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
+const client = new speech.SpeechClient();
 
+process.env.GOOGLE_APPLICATION_CREDENTIALS = "omnibot.json"
+
+const request = {
+    config:{
+        encoding: "MULAW",
+        sampleRateHertz: 8000,
+        languageCode: "en-GB"
+    },
+    interimResults: true,
+};
 server.listen(8080)
 console.log('Server listening on port 8080');
 
@@ -12,21 +25,30 @@ console.log('Server listening on port 8080');
 
 wss.on('connection', (ws)=>{
     console.log('WebSocket connected');
-    // Handle WebSocket messages here
+    let recognizeStream = null;
     ws.on("message",message =>{
         const msg =  JSON.parse(message);
         switch(msg.event){
             case "connected":
                 console.log('connected');
+                recognizeStream = client
+                .streamingRecognize(request)
+                .on("error",console.error)
+                .on("data",data=>{
+                    console.log(data.results[0].
+                    alternatives[0].transcript)
+                    });
                 break;
             case "start":
                 console.log('starting the media server');
                 break;
             case "media":
                 console.log('Recieving audio data');
+                recognizeStream.write(msg.media.payload);
                 break;
             case "stop":
                 console.log('Stopping the media server');
+                recognizeStream.destroy();
                 break;
         }
     })
